21f467ed90be67aecf5b3d53984f27177acc653e
Dexwin LTD <dexwinltd@gmail.com>
2019-08-06 10:51:10 +0000

    06/08 Monday

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -12,45 +12,45 @@ import HomeScreen from "../screens/HomeScreen";
 //start Up navigation start
 const StartUp = createStackNavigator(
   {
-    //onboarding screen
-    OnBoardingScreen: {
-      screen: OnBoardingScreen,
-      navigationOptions: {
-        gesturesEnabled: false
-      }
-    },
+    // //onboarding screen
+    // OnBoardingScreen: {
+    //   screen: OnBoardingScreen,
+    //   navigationOptions: {
+    //     gesturesEnabled: false
+    //   }
+    // },
 
-    //sign up screen
-    SignUpScreen: {
-      screen: SignUpScreen,
-      navigationOptions: {
-        gesturesEnabled: false
-      }
-    },
+    // //sign up screen
+    // SignUpScreen: {
+    //   screen: SignUpScreen,
+    //   navigationOptions: {
+    //     gesturesEnabled: false
+    //   }
+    // },
 
-    //sign In screen
-    SignInScreen: {
-      screen: SignInScreen,
-      navigationOptions: {
-        gesturesEnabled: false
-      }
-    },
+    // //sign In screen
+    // SignInScreen: {
+    //   screen: SignInScreen,
+    //   navigationOptions: {
+    //     gesturesEnabled: false
+    //   }
+    // },
 
-    //set up screen
-    SetUpScreen: {
-      screen: SetUpScreen,
-      navigationOptions: {
-        gesturesEnabled: false
-      }
-    },
+    // //set up screen
+    // SetUpScreen: {
+    //   screen: SetUpScreen,
+    //   navigationOptions: {
+    //     gesturesEnabled: false
+    //   }
+    // },
 
-    //forgot password screen
-    ForgotPasswordScreen: {
-      screen: ForgotPasswordScreen,
-      navigationOptions: {
-        gesturesEnabled: false
-      }
-    },
+    // //forgot password screen
+    // ForgotPasswordScreen: {
+    //   screen: ForgotPasswordScreen,
+    //   navigationOptions: {
+    //     gesturesEnabled: false
+    //   }
+    // },
 
     //home screen with contain child navigation
     HomeScreen: {
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { View, StatusBar } from "react-native";
 import { SafeAreaView } from "react-navigation";
-import AppStyles, { AppContainerStyle } from "../config/AppStyles";
+import AppStyles, { AppContainerStyles } from "../config/AppStyles";
 
 class HomeScreen extends Component {
   state = {};
@@ -9,13 +9,13 @@ class HomeScreen extends Component {
     return (
       <SafeAreaView
         forceInset={{ top: "always", bottom: "never" }}
-        style={AppContainerStyle.SafeAreaStyle}
+        style={AppContainerStyles.SafeAreaStyle}
       >
         <StatusBar
           backgroundColor={AppStyles.colors.primaryColor}
           barStyle="light-content"
         />
-        <View style={AppContainerStyle.MainView} />
+        <View style={AppContainerStyles.MainView} />
       </SafeAreaView>
     );
   }
